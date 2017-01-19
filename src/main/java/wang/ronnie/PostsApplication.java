96c9d931cc19bfe55e7e0ae8b936464bb7bd1253
ronniewang <ro_wsy@163.com>
2017-01-19 18:42:29 +0800

    add a new page

--- src/main/java/wang/ronnie/PostsApplication.java
+++ src/main/java/wang/ronnie/PostsApplication.java
@@ -2,23 +2,26 @@ package wang.ronnie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class PostsApplication {
 
+    //http://blog.codeleak.pl/2014/08/spring-mvc-test-assert-given-model-attribute-global-errors.html
     public static void main(String[] args) {
 
         SpringApplication.run(PostsApplication.class, args);
     }
 
-    @RequestMapping("/index")
-    @ResponseBody
-    public String index() {
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
 
-        return "hello";
+        model.addAttribute("name", name);
+        return "greeting";
     }
+
 }
--- src/test/java/wang/ronnie/PostsApplicationTests.java
+++ src/test/java/wang/ronnie/PostsApplicationTests.java
@@ -2,15 +2,29 @@ package wang.ronnie;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PostsApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void contextLoads() throws Exception {
+
+        mockMvc.perform(get("/greeting").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("name", "World"));
+    }
 
 }
